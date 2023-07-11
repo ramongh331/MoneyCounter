3b7ed49639f0c35cda3b5530dc747fea643ab76b
Ramon Hernandez <ramongh331@gmail.com>
2023-07-10 22:19:49 -0400

    added toggle between coins and bills and function to add them to total

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Penny from "../src/images/PennyTransparent.png";
 import Nickel from "../src/images/NickelTransparent.png";
 import Dime from "../src/images/DimeTransparent.png";
 import Quarter from "../src/images/QuarterTransparent.png";
+import One from "../src/images/1BillTransparent.png";
+import Five from "../src/images/5BillTransparent.png";
+import Ten from "../src/images/10BillTransparent.png";
+import Twenty from "../src/images/20BillTransparent.png";
 import {useState} from "react"
 
 function App() {
@@ -12,6 +16,13 @@ function App() {
   const [nickelCounter, setNickelCounter] = useState(0)
   const [dimeCounter, setDimeCounter] = useState(0)
   const [quarterCounter, setQuarterCounter] = useState(0)
+  
+  const [oneCounter, setOneCounter] = useState(0)
+  const [fiveCounter, setFiveCounter] = useState(0)
+  const [tenCounter, setTenCounter] = useState(0)
+  const [twentyCounter, setTwentyCounter] = useState(0)
+
+  const [coinsBills, setCoinsBills] = useState()
 
   function handleAddPenny(){
     setTotal(total +.01)
@@ -30,12 +41,37 @@ function App() {
     setQuarterCounter(quarterCounter + 1) 
   }
   
+  function handleAddOne(){
+    setTotal(total + 1)
+    setOneCounter(oneCounter + 1) 
+  }
+  function handleAddFive(){
+    setTotal(total + 5) 
+    setFiveCounter(fiveCounter + 1)
+  }
+  function handleAddTen(){
+    setTotal(total + 10)
+    setTenCounter(tenCounter + 1) 
+  }
+  function handleAddTwenty(){
+    setTotal(total + 20)
+    setTwentyCounter(twentyCounter + 1) 
+  }
+  
   function handleClearTotal(){
     setTotal(0) 
     setPennyCounter(0) 
     setNickelCounter(0) 
     setDimeCounter(0)
     setQuarterCounter(0) 
+    setOneCounter(0) 
+    setFiveCounter(0) 
+    setTenCounter(0) 
+    setTwentyCounter(0) 
+  }
+
+  function handleCoinsBills(){
+    setCoinsBills(!coinsBills)
   }
   
   
@@ -51,14 +87,14 @@ function App() {
             <button className="w-full text-white bg-orange-700 text-3xl p-3">
               Remove
             </button>
-            <button className="w-full text-white bg-orange-700 text-3xl p-3">
-              Coins/Bills
+            <button className="w-full flex justify-center items-center text-white bg-orange-700 text-3xl p-3" onClick={handleCoinsBills}>
+              {coinsBills ? <img className="h-12" src={One} alt="One Dollar Bill"/> : <img className="h-12" src={Quarter} alt="Quarter"/>}
             </button>
             <button className="w-full text-white bg-red-500 text-3xl p-3" onClick={handleClearTotal}>
               Clear
             </button>
           </section>
-          <section className="bg-yellow-400 w-full h-full">
+          {coinsBills ? <section className="w-full h-full">
             {/* Penny and Nickel Top Row */}
             <section className="bg-blue-400 w-full h-1/2 flex">
               <section className="w-1/2 h-full flex flex-col items-center">
@@ -98,7 +134,49 @@ function App() {
                 </section>
               </section>
             </section>
-          </section>
+          </section> : <section className="w-full h-full">
+            {/* One and Five Top Row */}
+            <section className="bg-blue-400 w-full h-1/2 flex">
+              <section className="w-1/2 h-full flex flex-col items-center">
+                <section className="bg-white w-full text-center text-2xl border-[1px] border-black">
+                  {oneCounter}
+                </section>
+                <section className="bg-gray-400 w-full h-full flex justify-center items-center border-black border-x-[1px] border-b-[1px] text-center" onClick={handleAddOne}>
+                  <img src={One} alt="One Dollar Bill" />
+                </section>
+              </section>
+              <section className="w-1/2 h-full flex flex-col items-center">
+                <section className="bg-white w-full text-center text-2xl border-y-[1px] border-r-[1px] border-black">
+                  {fiveCounter}
+                </section>
+                <section className="bg-gray-400 w-full h-full flex justify-center items-center border-black border-r-[1px] border-b-[1px] text-center" onClick={handleAddFive}>
+                  <img src={Five} alt="Five Dollar Bill" />
+                </section>
+              </section>
+            </section>
+
+            {/* Ten and Twenty Bottom Row */}
+            <section className="bg-blue-400 w-full h-1/2 flex">
+              <section className="w-1/2 h-full flex flex-col items-center">
+                <section className="bg-white w-full text-center text-2xl border-[1px] border-black">
+                  {tenCounter}
+                </section>
+                <section className="bg-gray-400 w-full h-full flex justify-center items-center border-black border-x-[1px] border-b-[1px] text-center" onClick={handleAddTen}>
+                  <img src={Ten} alt="Ten Dollar Bill" />
+                </section>
+              </section>
+              <section className="w-1/2 h-full flex flex-col items-center">
+                <section className="bg-white w-full text-center text-2xl border-y-[1px] border-r-[1px] border-black">
+                  {twentyCounter}
+                </section>
+                <section className="bg-gray-400 w-full h-full flex justify-center items-center border-black border-r-[1px] border-b-[1px] text-center" onClick={handleAddTwenty}>
+                  <img src={Twenty} alt="Twenty Dollar Bill" />
+                </section>
+              </section>
+            </section>
+          </section>}
+          
+          
           <section className="flex w-full h-[6rem]">
             <button className="w-full">Counter</button>
             <button className="w-full">Calculator</button>
